1c8eb532563507c90f6ec21898f2c0b95a76403a
woodwhales <woodwhales@163.com>
2022-01-19 17:28:39 +0800

    update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -86,7 +86,7 @@ module.exports = {
     },
     footer: { // 页脚信息
       createYear: 2022, // 博客创建年份
-      copyrightInfo: 'woodwhales | MIT License', // 博客版权信息，支持a标签
+      copyrightInfo: '<a href="https://woodwhales.cn" target="_blak">woodwhales.cn</a> | MIT License', // 博客版权信息，支持a标签
     },
     htmlModules,
   },
